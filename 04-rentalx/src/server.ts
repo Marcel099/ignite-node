5b4e066e3b81494b90f5e7660cf2dc27e679e8dc
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2022-03-01 20:22:49 -0300

    Cap. 2, M3 #3 - S2

--- 04-rentalx/src/server.ts
+++ 04-rentalx/src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+import { categoriesRoutes } from "./routes/categories.routes";
 
+const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+app.use(express.json());
 
+app.use("/categories", categoriesRoutes);
 
-app.listen(3333, () => console.log('Server is running!'))
+app.listen(3333, () => console.log("Server is running!"));
