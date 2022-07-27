867a8a3588615d2cbcdf10f29d50b0e635e0863b
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-07-27 12:05:49 -0300

    MWS, #1 - Configuração inicial do projeto

---
+++ bonus/websockets/src/http.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import path from "path";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("Socket", socket.id);
+});
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Hello Websocket",
+  })
+})
+
+export { server, io };
